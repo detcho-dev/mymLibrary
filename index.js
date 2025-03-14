59fdac471105915c7d810859cd349b9a80a33c63
detcho-dev <detcho476044028@gmail.com>
2025-03-14 17:50:02 +0300

    Update index.js

--- index.js
+++ index.js
@@ -40,3 +40,14 @@ function goToPurchasePage(bookName, bookPrice) {
         `;
     });
 });
+
+
+function sidebar(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hide(){
+    const sidebar = document.querySelector('.sidebar')
+    sidebar.style.display = 'none'
+}
